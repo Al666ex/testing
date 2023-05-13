ad722c0652358af4e2e5be21a05e2fe3ac2a9495
Alexandr Stavschi <alexandr.stavschi@asp.gov.md>
2023-05-13 15:43:41 +0300

    debounce

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import {usePosts} from './hooks/usePosts';
 import QueryString from './components/QueryString/QueryString';
 import Posts from './components/Posts/Posts';
+import { debounce } from './utils';
 
+const inputTimeout = 100
 function App() {    
-  const [query, setQuery] = useState('');
-  const users = usePosts(query);
+  const [query, setQuery] = useState('');  
+  const users = usePosts(query)
 
   return (
     <div className="App">
       <QueryString 
         query={query} 
-        setQuery={setQuery} 
+        setQuery={debounce(setQuery,inputTimeout)} 
       />
       <Posts 
         users={users}
--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,11 +1,12 @@
 import { useMemo } from "react";
 import {  useSelector } from 'react-redux';
 
+
 export const usePosts = (query) => {    
     const posts = useSelector(state => state.customers);             
     const rusultQuery = useMemo(() => 
         query.length > 2 ? 
-            [...posts].filter(item => item.toLowerCase().includes(query.toLowerCase())) : 
+        [...posts].filter(item => item.toLowerCase().includes(query.toLowerCase())) : 
                 []        
     ,[query,posts]) 
     return rusultQuery;
